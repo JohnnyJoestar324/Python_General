ec5b7ebab7e8ef2d871e916abfc2183ff7af499a
JohnnyJoestar324 <martinezjohnny324@gmail.com>
2025-08-27 18:44:41 -0600

    agregando practica con archivos de texto

---
+++ General/archivos.py
@@ -0,0 +1,11 @@
+import os
+# with open ('archivo.txt','x') as f:
+#     print(f)
+
+# with open('archivo.txt','w') as f:
+#     print(f.write("Escribiendo la primera linea de codigo"))
+
+# with open('archivo.txt', 'a') as f:
+#     f.writelines("\n la segunda linea en el archivo")
+
+os.remove('archivo.txt')
